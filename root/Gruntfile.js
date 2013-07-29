de01b299b73cce29a00a23a150046ddb8108c4d9
goliatone <burgosemi@gmail.com>
2013-07-29 15:12:00 -0400

    Added bump task.

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -141,6 +141,22 @@ module.exports = function (grunt) {
                     src: []
                 }]
             }
+        },
+        //https://github.com/vojtajina/grunt-bump
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json', 'component.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json', 'bower.json', 'component.json'], // '-a' for all files
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+            }
         }
     });
 

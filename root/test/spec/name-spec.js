09e02c5d436528fd3ebdf74de1990df577ec7ca3
goliatone <burgosemi@gmail.com>
2013-07-29 14:42:08 -0400

    Fix typo.

--- root/test/spec/name-spec.js
+++ root/test/spec/name-spec.js
@@ -8,13 +8,13 @@ define(['{%= name%}', 'jquery'], function({%= title%}, $) {
 
     describe('just checking', function() {
 
-        it('{%= title%} shold be loaded', function() {
+        it('{%= title%} should be loaded', function() {
             expect({%= title%}).toBeTruthy();
             var {%= name%} = new {%= title%}();
             expect({%= name%}).toBeTruthy();
         });
 
-        it('{%= title%} shold initialize', function() {
+        it('{%= title%} should initialize', function() {
             var {%= name%} = new {%= title%}();
             var output   = {%= name%}.init();
             var expected = 'This is just a stub!';

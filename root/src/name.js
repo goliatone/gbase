60259d1a2aa0208b810021710631e2aaff60fd80
goliatone <burgosemi@gmail.com>
2014-04-01 11:09:15 -0400

    Template: Base lib, updated extend method to handle deep clonning.

--- root/src/name.js
+++ root/src/name.js
@@ -40,20 +40,17 @@
      * @return {Object}        Resulting object from
      *                         meging target to params.
      */
-    var _extend = function(target) {
-        var i = 1, length = arguments.length, source;
-        for ( ; i < length; i++ ) {
-            // Only deal with defined values
-            if ((source = arguments[i]) != undefined ){
-                Object.getOwnPropertyNames(source).forEach(function(k){
-                    var d = Object.getOwnPropertyDescriptor(source, k) || {value:source[k]};
-                    if (d.get) {
-                        target.__defineGetter__(k, d.get);
-                        if (d.set) target.__defineSetter__(k, d.set);
-                    } else if (target !== d.value) target[k] = d.value;
-                });
+    var _extend= function extend(target) {
+        var sources = [].slice.call(arguments, 1);
+        sources.forEach(function (source) {
+            for (var property in source) {
+                if(source[property] && source[property].constructor &&
+                    source[property].constructor === Object){
+                    target[property] = target[property] || {};
+                    target[property] = extend(target[property], source[property]);
+                } else target[property] = source[property];
             }
-        }
+        });
         return target;
     };
 
